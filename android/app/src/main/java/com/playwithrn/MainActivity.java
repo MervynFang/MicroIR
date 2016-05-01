32718ac48d349110ca9a090a21ec824a4018a330
FangMervyn <mervynfang@gmail.com>
2016-05-01 23:50:08 +0800

    add image picker

--- android/app/src/main/java/com/playwithrn/MainActivity.java
+++ android/app/src/main/java/com/playwithrn/MainActivity.java
@@ -4,6 +4,8 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
+import com.imagepicker.ImagePickerPackage; // react-native-image-picker
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -35,6 +37,7 @@ public class MainActivity extends ReactActivity {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage()
+      , new ImagePickerPackage() // react-native-image-picker
       );
     }
 }
--- views/main.js
+++ views/main.js
@@ -9,27 +9,97 @@ import React, {
     Text,
     View,
     Alert,
+    ScrollView,
+    Image,
+    Platform,
     ToastAndroid,
     TouchableHighlight,
+    TouchableOpacity
+    // NativeModules:{
+    //     ImagePickerManager
+    // }
 } from 'react-native';
 
+var ImagePickerManager = require('NativeModules').ImagePickerManager;
+
 class Main extends Component {
+    state = {
+        selectedImage: null
+    };
+
+    selectImage() {
+        const options = {
+            title: 'Photo Picker',
+            takePhotoButtonTitle: 'Take Photo...',
+            chooseFromLibraryButtonTitle: 'Choose from Library...',
+            // quality: 0.5,
+            // maxWidth: 300,
+            // maxHeight: 300,
+            allowsEditing: true
+        };
+
+        ImagePickerManager.launchImageLibrary(options, (response) => {
+            console.log('Response = ', response);
+
+            if (response.didCancel) {
+                console.log('User cancelled photo picker');
+                // camera always be here
+            }
+            else if (response.error) {
+                console.log('ImagePickerManager Error: ', response.error);
+
+            }
+            else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+
+            }
+            else {
+                // You can display the image using either:
+                //const source = {uri: 'data:image/jpeg;base64,' + response.data, isStatic: true};
+
+                var source;
+                if (Platform.OS === 'android') {
+                    source = {uri: response.uri, isStatic: true};
+
+                } else {
+                    source = {uri: response.uri.replace('file://', ''), isStatic: true};
+                }
+
+                this.setState({
+                    selectedImage: source
+                });
+            }
+        });
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Learn How to Use Phone
-                </Text>
-                <TouchableHighlight
-                    underlayColor = '#fff'
-                    onPress={() => Alert.alert('Save Tips', 'Would You like to save the picture?', [
-                        {text: 'Three', onPress: () => ToastAndroid.show('click three', ToastAndroid.SHORT)},
-                        {text: 'YES', onPress: () => ToastAndroid.show('click one', ToastAndroid.SHORT)},
-                        {text: 'NO', onPress: () => ToastAndroid.show('click two', ToastAndroid.SHORT)}
-                    ])}>
-                    <Text style={{color: '#fff', fontSize: 30}}>The Button</Text>
-                </TouchableHighlight>
-            </View>
+            <ScrollView>
+                <View style={styles.container}>
+                    <Text style={styles.welcome}>
+                        Learn How to Use Phone
+                    </Text>
+                    <TouchableOpacity
+                        onPress={this.selectImage.bind(this)}>
+                        <Text style={styles.desp}>Select Image</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => Alert.alert('Save Tips', 'Would You like to save the picture?', [
+                            {text: 'Three', onPress: () => ToastAndroid.show('click three', ToastAndroid.SHORT)},
+                            {text: 'YES', onPress: () => ToastAndroid.show('click one', ToastAndroid.SHORT)},
+                            {text: 'NO', onPress: () => ToastAndroid.show('click two', ToastAndroid.SHORT)}
+                        ])}>
+                        <Text style={styles.desp}>The Button</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={this.selectImage.bind(this)}>
+                        {this.state.selectedImage === null
+                            ? <Text style={styles.selectdesp}>请选择图片</Text>
+                        : <Image source={this.state.selectedImage} style={styles.pic}></Image>}
+                    </TouchableOpacity>
+                    <Image source={require('../images/point.png')} style={styles.pic}></Image>
+                </View>
+            </ScrollView>
         );
     }
 }
@@ -47,6 +117,22 @@ const styles = StyleSheet.create({
         margin: 10,
         color: '#FFF',
     },
+    pic: {
+        width: 300,
+        height: 300
+    },
+    desp: {
+        color: '#fff',
+        fontSize: 30
+    },
+    selectdesp: {
+        width: 300,
+        height: 300,
+        fontSize: 30,
+        textAlign: 'center',
+        color: '#808080',
+        backgroundColor: '#F2F2F5'
+    }
 });
 
 export {Main};
