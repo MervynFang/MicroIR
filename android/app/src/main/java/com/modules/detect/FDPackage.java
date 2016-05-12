6fc6a94934f79fe65e1dbc125f0b5b0c479562e0
FangMervyn <mervynfang@gmail.com>
2016-05-13 00:27:59 +0800

    add face detection

---
+++ android/app/src/main/java/com/modules/detect/FDPackage.java
@@ -0,0 +1,7 @@
+/**
+* @Author: Mervyn
+* @Date:   2016,May,12 22:58:43
+* @Last modified by:   Mervyn
+* @Last modified time: 2016,May,12 22:59:35
+*/
+
---
+++ views/cam.js
@@ -0,0 +1,30 @@
+/**
+* @Author: Mervyn
+* @Date:   2016,May,12 22:53:28
+* @Last modified by:   Mervyn
+* @Last modified time: 2016,May,12 22:59:24
+*/
+
+import React, {
+    Component,
+    StyleSheet,
+    Text,
+    View,
+    Alert,
+    ScrollView,
+    Image,
+    Platform,
+    ToastAndroid,
+    TouchableHighlight,
+    TouchableOpacity
+} from 'react-native';
+
+import {styles} from '../styles/styles';
+
+class Cam extends Component {
+    render() {
+        
+    }
+}
+
+export {Main};
