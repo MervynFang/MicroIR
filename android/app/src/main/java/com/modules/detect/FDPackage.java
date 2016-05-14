9d9c1cefbf70d05b7d4b0771bfbbd19dc8a94754
FangMervyn <mervynfang@gmail.com>
2016-05-14 23:35:16 +0800

    add packge

--- android/app/src/main/java/com/modules/detect/FDPackage.java
+++ android/app/src/main/java/com/modules/detect/FDPackage.java
@@ -2,6 +2,24 @@
 * @Author: Mervyn
 * @Date:   2016,May,12 22:58:43
 * @Last modified by:   Mervyn
-* @Last modified time: 2016,May,12 22:59:35
+* @Last modified time: 2016,May,14 23:34:35
 */
 
+public class FDPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> creatNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new FaceDetectionModule(reactContext));
+        return modules;
+    }
+    
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
