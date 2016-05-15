318a2e27a6156327770f623c595ac968047fbf68
FangMervyn <mervynfang@gmail.com>
2016-05-15 13:32:24 +0800

    add file

---
+++ android/app/src/main/java/com/modules/detect/FdPackage.java
@@ -0,0 +1,38 @@
+/**
+* @Author: Mervyn
+* @Date:   2016,May,15 13:32:04
+* @Last modified by:   Mervyn
+* @Last modified time: 2016,May,15 13:32:11
+*/
+
+package com.modules.detect;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class FdPackage implements ReactPackage {
+    // @Override
+    public List<NativeModule> creatNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new FaceDetectionModule(reactContext));
+        return modules;
+    }
+    
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
