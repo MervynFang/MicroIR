648c14fb2614c5713f00ce50ff93aacd52cb5bb8
FangMervyn <mervynfang@gmail.com>
2016-07-24 16:17:24 +0800

    Modified IOS

--- index.ios.js
+++ index.ios.js
@@ -2,7 +2,7 @@
 * @Author: Mervyn
 * @Date:   2016,Feb,27 21:47:14
 * @Last modified by:   Mervyn
-* @Last modified time: 2016,Jul,23 22:33:43
+* @Last modified time: 2016,Jul,24 16:16:54
 */
 
 /**
@@ -11,52 +11,22 @@
  */
 'use strict';
 import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    Component,
+    BackAndroid,
+    ToastAndroid,
+    // StyleSheet,
+    // Text,
+    // View
 } from 'react-native';
 
 import {Main} from './views/main';
 import {Pop} from './views/pop';
 
 class playwithrn extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+      
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('playwithrn', () => playwithrn);
