80362d953a384160528b69825e16863e77d20e46
FangMervyn <mervynfang@gmail.com>
2016-07-31 01:17:19 +0800

    Add 0.30 new file

---
+++ android/app/src/main/java/com/playwithrn/MainApplication.java
@@ -0,0 +1,44 @@
+package com.playwithrn;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.imagepicker.ImagePickerPackage; // react-native-image-picker
+import com.github.xinthink.rnmk.ReactMaterialKitPackage; // react-native-material-kit
+import com.modules.screen.ScreenPackage; // face detection module
+import com.RNFetchBlob.RNFetchBlobPackage;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    protected boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage()
+          new ImagePickerPackage(), // react-native-image-picker
+          new ReactMaterialKitPackage(), // react-native-material-kit
+          new ScreenPackage(), // screen module
+          new RNFetchBlobPackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+      return mReactNativeHost;
+  }
+}
